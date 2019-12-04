4a4d6580d8f5d547c82a22dcd9973994d4e88921
coltynw <coltynw@users.github.com>
2019-12-03 20:15:05 -0500

    got it working

--- ghcard/src/App.js
+++ ghcard/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import User from "./components/User";
+import Followers from "./components/Followers"
 
 function App() {
+
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <h1>GitHub User Card</h1>
+          <User  />
+          <Followers />
+
+
     </div>
   );
 }
 
-export default App;
+export default App; 
